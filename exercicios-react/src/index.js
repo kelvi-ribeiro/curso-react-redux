9403570f3425b6cbb45f6a50ba4bdf45929ff917
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-14 23:03:02 -0300

    Aula 13. Primeiro Componente

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimeiroComponente from './components/PrimeiroComponente'
+
 
 const elemento = document.getElementById('root')
 
-ReactDOM.render(<h1>Olá React</h1>,elemento)
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente/>
+    </div>
+    ,elemento)
