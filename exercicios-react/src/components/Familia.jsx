83676a7c42e485af5a86b201c0f0d7e49ca32cb0
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-15 02:34:29 -0300

    Aula 20. Relacao entre componentes 02

--- exercicios-react/src/components/Familia.jsx
+++ exercicios-react/src/components/Familia.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Membro from './Membro'
+
 
 
 export default props =>
     <div>
-        <Membro nome="Rafael" sobrenome={props.familia}/>
-        <Membro nome="Daniela" sobrenome={props.familia}/>
-        <Membro nome="Pedro" sobrenome={props.familia}/>
-        <Membro nome="Ana" sobrenome={props.familia}/>
+        <h1>Família</h1>
+        {props.children}
     </div>
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Familia from './components/Familia';
+import Familia from './components/Familia'
+import Membro from './components/Membro'
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <Familia familia="Silva"/>
+        <Familia>
+            <Membro nome="Kelvi" sobrenome="Ribeiro" />
+            <Membro nome="Thais" sobrenome="Ribeiro" />
+        </Familia>
+        <Familia>
+            <Membro nome="Ned" sobrenome="Stark" />
+            <Membro nome="Sansa" sobrenome="Stark" />
+        </Familia>
     </div>
-    ,elemento)
+    , elemento)
