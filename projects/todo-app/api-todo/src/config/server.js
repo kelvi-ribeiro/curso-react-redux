7743ef048f2c130703ad6fdefbaed592e6443137
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-21 11:56:09 -0300

    Fazendo import da instancia de server

--- projects/todo-app/api-todo/src/config/server.js
+++ projects/todo-app/api-todo/src/config/server.js
@@ -7,6 +7,9 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended:true }))
 server.use(bodyParser.json())
 
+
 server.listen(port,function(){
     console.log('BACKEND is running on port:' + port)
 })
+
+module.exports = server
