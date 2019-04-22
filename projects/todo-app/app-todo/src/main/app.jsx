8ede415bf97c718c3f681e461798c98d726217a5
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-21 23:36:35 -0300

    Aula 55. Criando o componente Menu

--- projects/todo-app/app-todo/src/main/app.jsx
+++ projects/todo-app/app-todo/src/main/app.jsx
@@ -4,9 +4,11 @@ import 'font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Todo from '../todo/Todo';
 import About from '../about/About';
+import Menu from '../template/Menu';
 
 export default () => (
     <div className="container">
+    <Menu/>
     <Todo/>    
     <About/>
     </div>
