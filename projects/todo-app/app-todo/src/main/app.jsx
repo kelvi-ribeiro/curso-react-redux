15bf53996f9512ad1ee9342eb80ee1773ad266df
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-21 23:23:16 -0300

    Aula 53. Componente App

---
+++ projects/todo-app/app-todo/src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default () => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
