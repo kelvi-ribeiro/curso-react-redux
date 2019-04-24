cf0dc897e578b5fac77e73f75dd2257ac121cc2a
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-24 05:14:09 -0300

    Mudando nome do componente If para Show e removendo import desnecessario

--- projects/todo-app/app-todo/src/template/IconButton.jsx
+++ projects/todo-app/app-todo/src/template/IconButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import If from './If'
+import Show from './Show'
 export default props => {    
     return (
-        <If show={props.show === undefined ? true : props.show}>        
+        <Show show={props.show === undefined ? true : props.show}>        
             <button className={`btn btn-${props.style}`} onClick={props.onClick}>
                 <i className={`fa fa-${props.icon}`}></i>
             </button>
-        </If>
+        </Show>
     )
 }
