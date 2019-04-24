d4d46dd6eb440da598b32398c9c39d3d0b7bf6f5
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-24 00:46:54 -0300

    Aula 64. Criando metodo handleChange

--- projects/todo-app/app-todo/src/todo/Todo.jsx
+++ projects/todo-app/app-todo/src/todo/Todo.jsx
@@ -6,16 +6,24 @@ import TodoForm from './TodoForm';
 export default class Todo extends Component {
     constructor(props){
         super(props)
+        this.state = { 
+            description:'',
+            list:[]
+        }
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
     handleAdd() {
         console.log(this)
+    }
+    handleChange(e) {
+        this.setState({description:e.target.value})
     }
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <TodoForm handleAdd={this.handleAdd} />
+                <TodoForm handleChange={this.handleChange} description={this.state.description} handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
--- projects/todo-app/app-todo/src/todo/TodoForm.jsx
+++ projects/todo-app/app-todo/src/todo/TodoForm.jsx
@@ -6,7 +6,9 @@ export default props => (
     <div role="form" className="todo-form">
         <Grid cols="12 9 10">
             <input type="text" id="description"
-                className="form-control" placeholder="Adicione uma tarefa" />
+                   className="form-control" placeholder="Adicione uma tarefa"
+                   onChange={props.handleChange}
+                   value={props.description} />
         </Grid>
         <Grid cols="12 3 2">
             <IconButton show={true} style="primary" icon="plus" onClick={props.handleAdd}/>
