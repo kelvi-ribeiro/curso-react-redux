a48629f5790e9ec536cbbe15a1d9311f72da70f1
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-24 05:26:27 -0300

    Melhorando casos de lista de todos vazia
    Exibindo alert quando tiver nenhum todo cadastrado e outro alert para quando a pesquisa retornar uma lista vazia de todos

--- projects/todo-app/app-todo/src/todo/Todo.jsx
+++ projects/todo-app/app-todo/src/todo/Todo.jsx
@@ -4,66 +4,76 @@ import axios from 'axios'
 import PageHeader from '../template/PageHeader';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import Show from '../template/Show';
+import Alert from '../template/Alert';
 
 const URL = 'http://localhost:3000/api/todos'
 
 export default class Todo extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = { 
-            description:'',
-            list:[]
+        this.state = {
+            description: '',
+            list: []
         }
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
-        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)        
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleClear = this.handleClear.bind(this)
         this.refresh()
     }
 
-    refresh(description = ''){        
-        const search = description ? `&description__regex=/${description}/`:''
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
         axios.get(`${URL}?sort=-createdAt${search}`)
-        .then(res => this.setState({description,list:res.data}))
+            .then(res => this.setState({ description, list: res.data }))
     }
-   
+
     handleAdd() {
-        axios.post(URL,{description:this.state.description})
-        .then(() => this.refresh())
+        axios.post(URL, { description: this.state.description })
+            .then(() => this.refresh())
     }
 
     handleChange(e) {
-        this.setState({description:e.target.value},() => this.refresh(this.state.description))
+        this.setState({ description: e.target.value }, () => this.refresh(this.state.description))
     }
 
-    handleRemove(id){
+    handleRemove(id) {
         axios.delete(`${URL}/${id}`)
-        .then(() => this.refresh(this.state.description))
+            .then(() => this.refresh(this.state.description))
     }
 
-    handleMarkAsDone(todo){
-        axios.put(`${URL}/${todo._id}`,{...todo, done:true})
-        .then(() => this.refresh(this.state.description))
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(() => this.refresh(this.state.description))
     }
-    handleMarkAsPending(todo){
-        axios.put(`${URL}/${todo._id}`,{...todo, done:false})
-        .then(() => this.refresh(this.state.description))
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(() => this.refresh(this.state.description))
     }
 
-    handleClear(){
+    handleClear() {
         this.refresh()
-    }    
+    }
 
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <TodoForm handleChange={this.handleChange}  description={this.state.description}
-                          handleAdd={this.handleAdd}  handleClear={this.handleClear} />
-                <TodoList handleRemove={this.handleRemove} list={this.state.list}
-                          handleMarkAsDone={this.handleMarkAsDone}  handleMarkAsPending={this.handleMarkAsPending}      />
+                <TodoForm handleChange={this.handleChange} description={this.state.description}
+                    handleAdd={this.handleAdd} handleClear={this.handleClear} />
+                <Show show={this.state.list.length > 0}>
+                    <TodoList handleRemove={this.handleRemove} list={this.state.list}
+                        handleMarkAsDone={this.handleMarkAsDone} handleMarkAsPending={this.handleMarkAsPending} />
+                </Show>
+                <Show show={this.state.list.length === 0 && !this.state.description.length}>
+                   <Alert style="warning" message="Nenhum TODO cadastrado"/>
+                </Show>
+                <Show show={this.state.list.length === 0 && this.state.description.length}>
+                   <Alert style="warning" message="Nenhum foi TODO foi encontrado"/>
+                </Show>
             </div>
         )
     }
