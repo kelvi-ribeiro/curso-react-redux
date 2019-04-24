5781915ea51b8ae890b4dca531eaa0543c48f987
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-24 04:13:53 -0300

    Impedindo que o atributo show seja obrigatorio

--- projects/todo-app/app-todo/src/template/IconButton.jsx
+++ projects/todo-app/app-todo/src/template/IconButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import If from './If'
-export default props => {
+export default props => {    
     return (
-        <If show={props.show}>
+        <If show={props.show === undefined ? true : props.show}>        
             <button className={`btn btn-${props.style}`} onClick={props.onClick}>
                 <i className={`fa fa-${props.icon}`}></i>
             </button>
