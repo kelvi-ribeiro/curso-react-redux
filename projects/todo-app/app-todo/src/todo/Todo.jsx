5706ef043cead263a18850481f63d692fea64105
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-24 04:14:09 -0300

    Aula 66. Consulta e Exclusao de TODOS

--- projects/todo-app/app-todo/src/todo/Todo.jsx
+++ projects/todo-app/app-todo/src/todo/Todo.jsx
@@ -16,20 +16,35 @@ export default class Todo extends Component {
         }
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
     }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+        .then(res => this.setState({description:'',list:res.data}))
+    }
+
     handleAdd() {
         axios.post(URL,{description:this.state.description})
-        .then(res => console.log(res))
+        .then(() => this.refresh())
     }
+
     handleChange(e) {
         this.setState({description:e.target.value})
     }
+
+    handleRemove(id){
+        axios.delete(`${URL}/${id}`)
+        .then(() => this.refresh())
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
                 <TodoForm handleChange={this.handleChange} description={this.state.description} handleAdd={this.handleAdd} />
-                <TodoList />
+                <TodoList handleRemove={this.handleRemove} list={this.state.list} />
             </div>
         )
     }
