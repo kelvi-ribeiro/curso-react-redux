897f99ee5d30006e97c31c2a96ff2de3491b2bae
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-02 00:41:06 -0300

    Implementando pesquisa no onChange do input com o Redux

--- projects/todo-app/app-todo/src/todo/todoActions.js
+++ projects/todo-app/app-todo/src/todo/todoActions.js
@@ -2,13 +2,21 @@ import axios from 'axios'
 
 const URL = 'http://localhost:3000/api/todos'
 
-export const changeDescription = event => ({
-    type: 'DESCRIPTION_CHANGED',
-    payload: event.target.value
-})
+export const changeDescription = event => {
+    return dispatch => {
+        dispatch({type:'DESCRIPTION_CHANGED',payload:event.target.value})
+        dispatch(search(event.target.value))
+        
+    }
+}
 
-export const search = () => {
-    const request = axios.get(`${URL}?sort=-createdAt`)    
+export const search = (description) => {    
+    let search = ''
+    if(description){
+        search = `&description__regex=/${description }/`
+    }
+    
+    const request = axios.get(`${URL}?sort=-createdAt${search}`)
     return {
         type:'TODO_SEARCHED',
         payload:request
