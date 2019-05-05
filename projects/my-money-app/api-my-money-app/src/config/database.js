abec50ada75d062edee9a8548afc2458e0361176
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-05 16:09:08 -0300

    Aula102. Alterações nas Mensagens de Erro

--- projects/my-money-app/api-my-money-app/src/config/database.js
+++ projects/my-money-app/api-my-money-app/src/config/database.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é Obrigatório."
+
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite mínimo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
