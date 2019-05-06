524aa1bed84e37282f5d40e7f4cc9e5a966431d2
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-05 21:28:44 -0300

    Aula 103. Obtendo a Quantidade de Registros(Count)

--- projects/my-money-app/api-my-money-app/src/api/services/billingCycleService.js
+++ projects/my-money-app/api-my-money-app/src/api/services/billingCycleService.js
@@ -4,4 +4,14 @@ billingCycle.methods(['get','post','put','delete'])
 
 billingCycle.updateOptions({ new:true,  runValidators:true })
 
+billingCycle.route('count',(req, res, next) => {
+    billingCycle.count((error, value) => {
+        if(error){
+            res.status(500).json({erors:[error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
 module.exports = billingCycle
