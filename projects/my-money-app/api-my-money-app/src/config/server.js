0b80843f15ec0af912d66d0af5a67f432f4f3ee1
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-05 23:31:55 -0300

    Aula 107. Middleware Query Parser Int

--- projects/my-money-app/api-my-money-app/src/config/server.js
+++ projects/my-money-app/api-my-money-app/src/config/server.js
@@ -3,10 +3,12 @@ const port = 3000
 const bodyParser = require('body-parser')
 const server = require('express')()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended:true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 
 
