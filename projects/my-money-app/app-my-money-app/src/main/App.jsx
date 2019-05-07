5025ce9ad6fd86bfb843529dae2cd7256d61a3c7
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-07 00:01:52 -0300

    Aula 115. Componente Cabeçalho

--- projects/my-money-app/app-my-money-app/src/main/App.jsx
+++ projects/my-money-app/app-my-money-app/src/main/App.jsx
@@ -1,8 +1,9 @@
 import '../common/template/dependencies'
-import { React } from "react";
+import  React  from "react";
+import Header from '../common/template/Header';
 
 export default props => (
     <div className="wrapper">
-        <h1>App</h1>
+        <Header />
     </div>
 )
