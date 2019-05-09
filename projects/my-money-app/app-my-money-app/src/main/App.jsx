1040d965382189deca02ec96876013f6c83f0d66
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-08 22:08:30 -0300

    Aula 119. Navegação com React-Router

--- projects/my-money-app/app-my-money-app/src/main/App.jsx
+++ projects/my-money-app/app-my-money-app/src/main/App.jsx
@@ -3,13 +3,14 @@ import  React  from "react";
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
 import Footer from '../common/template/Footer';
+import Routers from './Routers';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
         <div className="content-wrapper">
-        <h1>Conteúdo</h1>
+        <Routers/>
         </div>
         <Footer/>
     </div>
