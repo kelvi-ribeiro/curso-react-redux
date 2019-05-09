665acb73f5950976c671a0e68acad5e1f9383434
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-08 22:13:59 -0300

    Aula 120. Componente de Título do Conteúdo

--- projects/my-money-app/app-my-money-app/src/dashboard/Dashboard.js
+++
@@ -1,7 +0,0 @@
-  import React from 'react'
-
-  export default props => (
-      <div>
-          <h1>Dashboard</h1>
-      </div>
-  )
---
+++ projects/my-money-app/app-my-money-app/src/dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/ContentHeader';
+
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title="Dashboard" subtitle="Versão 1.0"/>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
