134dcb76a50559d9f621aad8508866d90a9a0e86
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-08 21:45:08 -0300

    Aula 118. Criando o Componente Rodapé

--- projects/my-money-app/app-my-money-app/src/main/App.jsx
+++ projects/my-money-app/app-my-money-app/src/main/App.jsx
@@ -2,10 +2,15 @@ import '../common/template/dependencies'
 import  React  from "react";
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
+import Footer from '../common/template/Footer';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
+        <div className="content-wrapper">
+        <h1>Conteúdo</h1>
+        </div>
+        <Footer/>
     </div>
 )
