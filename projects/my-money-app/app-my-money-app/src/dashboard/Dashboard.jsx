792f6cbc3d1a225223192f2654ca4d2a56159011
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-12 12:43:57 -0300

    Aula 124. Usando ValueBox

--- projects/my-money-app/app-my-money-app/src/dashboard/Dashboard.jsx
+++ projects/my-money-app/app-my-money-app/src/dashboard/Dashboard.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import ContentHeader from '../common/template/ContentHeader';
+import Content from '../common/template/Content';
+import ValueBox from '../common/widget/ValueBox';
+import Row from '../common/layout/Row';
 
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <ContentHeader title="Dashboard" subtitle="Versão 1.0"/>
+                <ContentHeader title="Dashboard" subtitle="Versão 1.0" />
+                <Content>
+                    <Row>
+                        <ValueBox cols="12 4" color="green" icon="bank"
+                            value="R$ 10" text="Total de Créditos" />
+                        <ValueBox cols="12 4" color="red" icon="credit-card"
+                            value="R$ 10" text="Total de Débitos" />
+                        <ValueBox cols="12 4" color="blue" icon="money"
+                            value="0" text="Valor Consolidado" />
+                    </Row>
+                </Content>
             </div>
         )
     }
