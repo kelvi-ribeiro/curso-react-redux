0090332981b0d6574390f6beadcd326982332fec
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-12 19:34:59 -0300

    Aula 134. Componente TabHeader

--- projects/my-money-app/app-my-money-app/src/billingCycle/BillingCycle.jsx
+++ projects/my-money-app/app-my-money-app/src/billingCycle/BillingCycle.jsx
@@ -4,6 +4,7 @@ import Content from '../common/template/Content';
 import Tabs from '../common/tab/Tabs';
 import TabsHeader from '../common/tab/TabsHeader';
 import TabsContent from '../common/tab/TabsContent';
+import TabHeader from '../common/tab/TabHeader';
 
 export default class BillingCycle extends Component {
     render() {
@@ -13,7 +14,10 @@ export default class BillingCycle extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader label="Listar" icon="bars" target="tabList"/>
+                            <TabHeader label="Incluir" icon="plus" target="tabCreate"/>
+                            <TabHeader label="Alterar" icon="pencil" target="tabUpdate"/>
+                            <TabHeader label="Excluir" icon="trash-o" target="tabDelete"/>
                         </TabsHeader>
                         <TabsContent>
 
