80a915488c911656f0505526ca634084c195302a
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-12 19:27:11 -0300

    Aula 133. Componente Estruturais  das Abas

--- projects/my-money-app/app-my-money-app/src/billingCycle/BillingCycle.jsx
+++ projects/my-money-app/app-my-money-app/src/billingCycle/BillingCycle.jsx
@@ -1,14 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ContentHeader from '../common/template/ContentHeader';
 import Content from '../common/template/Content';
+import Tabs from '../common/tab/Tabs';
+import TabsHeader from '../common/tab/TabsHeader';
+import TabsContent from '../common/tab/TabsContent';
 
 export default class BillingCycle extends Component {
-    render(){
+    render() {
         return (
             <div>
                 <ContentHeader title="Ciclos de Pagamentos" subtitle="Cadastro" />
                 <Content>
-                    Ciclo de Pagamentos
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
