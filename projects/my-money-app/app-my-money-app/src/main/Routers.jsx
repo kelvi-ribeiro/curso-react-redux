3cd2cf233ad725a86610681452ba954774bf4406
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-12 19:14:35 -0300

    Aula 130. Criando o Componente Dashboard SEM Redux

--- projects/my-money-app/app-my-money-app/src/main/Routers.jsx
+++ projects/my-money-app/app-my-money-app/src/main/Routers.jsx
@@ -3,10 +3,11 @@ import {Router, Route, Redirect, hashHistory} from 'react-router'
 
 import Dashboard from '../dashboard/Dashboard'
 import BillingCycle from '../billingCycle/BillingCycle'
+import Dashboard2 from '../dashboard2/Dashboard2';
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path="/" component={Dashboard} />
+        <Route path="/" component={Dashboard2} />
         <Route path="/billingCycles" component={BillingCycle} />
         <Redirect from="*" to="/"  />
     </Router>
