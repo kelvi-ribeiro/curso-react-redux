e020b28bb6e8dd613b336f9c07d7c098cc9050be
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-13 11:48:48 -0300

    Aula 136. Componente TabHeader: Conectando com Redux

--- projects/my-money-app/app-my-money-app/src/common/tab/TabHeader.jsx
+++ projects/my-money-app/app-my-money-app/src/common/tab/TabHeader.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { selectTab } from './tabActions'
 
-export default class TabHeader extends Component {
+class TabHeader extends Component {
     render(){
+        const selected = this.props.tab.selected === this.props.target
         return (
-            <li>
+            <li className={selected ? 'active' : ''}>
                 <a href="javascript:;"
                 data-toggle="tab" 
+                onClick={() => this.props.selectTab(this.props.target)}
                 data-target={this.props.target}>
                 <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
                 </a>
@@ -13,3 +18,11 @@ export default class TabHeader extends Component {
         )
     }
 }
+
+const mapStateToProps = state => ({
+    tab: state.tab
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+
+export default connect(mapStateToProps,mapDispatchToProps)(TabHeader)
--- projects/my-money-app/app-my-money-app/src/common/tab/tabActions.js
+++ projects/my-money-app/app-my-money-app/src/common/tab/tabActions.js
@@ -1,5 +1,4 @@
-export function selectTab(tabId){
-    console.log(tabId)
+export function selectTab(tabId){    
     return {
         type:'TAB_SELECTED',
         payload:tabId
