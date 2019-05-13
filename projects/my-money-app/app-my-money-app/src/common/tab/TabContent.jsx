8ca5f10ef28f4c9878db303668d5dabba36092e6
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-13 14:05:12 -0300

    Aula 140. Visibilidade das Abas: Parte 2

--- projects/my-money-app/app-my-money-app/src/common/tab/TabContent.jsx
+++ projects/my-money-app/app-my-money-app/src/common/tab/TabContent.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import Show from '../operador/Show';
 class TabContent extends Component {
     render() {
         const selected = this.props.tab.selected === this.props.id
+        const visible = this.props.tab.visible[this.props.id]
         return (
-            <div id={this.props.id} className={`tab-pane ${selected && 'active'}`}>
-                {this.props.children}
-            </div>
+            <Show show={visible} >
+                <div id={this.props.id} className={`tab-pane ${selected && 'active'}`}>
+                    {this.props.children}
+                </div>
+            </Show>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    tab:state.tab
+    tab: state.tab
 })
 
 
--- projects/my-money-app/app-my-money-app/src/common/tab/TabHeader.jsx
+++ projects/my-money-app/app-my-money-app/src/common/tab/TabHeader.jsx
@@ -1,20 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { selectTab } from './tabActions'
+import Show from '../operador/Show';
 
 class TabHeader extends Component {
-    render(){
+    render() {
         const selected = this.props.tab.selected === this.props.target
+        const visible = this.props.tab.visible[this.props.target]
         return (
-            <li className={selected && 'active'}>
-                <a href="javascript:;"
-                data-toggle="tab" 
-                onClick={() => this.props.selectTab(this.props.target)}
-                data-target={this.props.target}>
-                <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
-                </a>
-            </li>
+            <Show show={visible}>
+                <li className={selected && 'active'}>
+                    <a href="javascript:;"
+                        data-toggle="tab"
+                        onClick={() => this.props.selectTab(this.props.target)}
+                        data-target={this.props.target}>
+                        <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+                    </a>
+                </li>
+            </Show>
         )
     }
 }
@@ -25,4 +29,4 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
 
-export default connect(mapStateToProps,mapDispatchToProps)(TabHeader)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
