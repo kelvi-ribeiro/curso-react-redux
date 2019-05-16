d6c50aafd23c91f7e77e30cc6b4afd08b1bf37ba
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-16 09:59:22 -0300

    Aula 149. Concateando actions na action de createBillingCycle através do redux-multi e redux-thunk

--- projects/my-money-app/app-my-money-app/src/billingCycle/billingCycleActions.js
+++ projects/my-money-app/app-my-money-app/src/billingCycle/billingCycleActions.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
-import { toastr } from "react-redux-toastr";
+import { toastr } from "react-redux-toastr"
+import { reset as resetForm } from "redux-form";
+import {showTabs, selectTab} from '../common/tab/tabActions'
 const BASE_URL = 'http://localhost:3000/api'
+
 
 export function getList(){
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -11,14 +14,24 @@ export function getList(){
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    .then(() => toastr.success('Sucesso','Operação Realizada com sucesso'))
-    .catch((e) => {
-        e.response.data.errors.forEach(error => {
-            toastr.error('Erro',error)
-        });
-    })
-    return {
-        type:'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(() => {
+            toastr.success('Sucesso','Operação Realizada com sucesso')
+            dispatch([
+                resetForm('billingCycleForm'),
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList',' tabCreate')
+            ])
+            
+        })
+        .catch((e) => {
+            e.response.data.errors.forEach(error => {
+                toastr.error('Erro',error)
+            })
+        })        
     }
+    
+    
 }
--- projects/my-money-app/app-my-money-app/src/main/reducers.js
+++ projects/my-money-app/app-my-money-app/src/main/reducers.js
@@ -1,9 +1,9 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
+import { combineReducers } from "redux"
+import { reducer as formReducer } from "redux-form"
 import {reducer as toastrReducer} from 'react-redux-toastr'
 import dashboardReducer from '../dashboard/dashboardReducer'
-import tabReducer from "../common/tab/tabReducer";
-import billingCycleReducer from "../billingCycle/billingCycleReducer";
+import tabReducer from "../common/tab/tabReducer"
+import billingCycleReducer from "../billingCycle/billingCycleReducer"
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
