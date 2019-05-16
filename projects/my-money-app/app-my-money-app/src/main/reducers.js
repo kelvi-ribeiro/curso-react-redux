349f290c65316321f834010cb6ccc2227e0dbdd8
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-16 01:32:28 -0300

    Aula 142. Criando a Action "getList" e o Reducer

--- projects/my-money-app/app-my-money-app/src/main/reducers.js
+++ projects/my-money-app/app-my-money-app/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import dashboardReducer from '../dashboard/dashboardReducer'
 import tabReducer from "../common/tab/tabReducer";
+import billingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
-    tab:tabReducer
+    tab:tabReducer,
+    billingCycle:billingCycleReducer
 })
 
 
