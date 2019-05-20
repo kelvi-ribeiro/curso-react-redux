eeb31e8284ffbd165c25e84b991ecae67a4bbbe0
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-19 22:27:07 -0300

    Aula 161. Implemetando a função de remover Créditos

--- projects/my-money-app/app-my-money-app/src/billingCycle/CreditList.jsx
+++ projects/my-money-app/app-my-money-app/src/billingCycle/CreditList.jsx
@@ -1,7 +1,7 @@
 import React,{ Component } from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
-import { Field, arrayInsert } from "redux-form"
+import { Field, arrayInsert, arrayRemove } from "redux-form"
 import Grid from '../common/layout/Grid'
 import Input from '../common/form/Input'
 
@@ -11,6 +11,12 @@ class CreditList extends Component {
             this.props.arrayInsert('billingCycleForm','credits', index, item)
         }
     }
+    remove(index){
+        if(!this.props.readOnly && this.props.list.length > 1){
+            this.props.arrayRemove('billingCycleForm','credits', index)
+        }
+    }
+
     renderRows(){
         const list = this.props.list || []
         return list.map((item,index)=>(
@@ -35,6 +41,10 @@ class CreditList extends Component {
                     <button type="button" className="btn btn-warning"
                     onClick={() => this.add(index + 1, item)}>
                     <i className="fa fa-clone"></i>
+                    </button>
+                    <button type="button" className="btn btn-danger"
+                    onClick={() => this.remove(index)}>
+                    <i className="fa fa-trash-o"></i>
                     </button>
                 </td>
             </tr>
@@ -65,6 +75,6 @@ class CreditList extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert,arrayRemove }, dispatch)
 
 export default connect(null,mapDispatchToProps)(CreditList)
