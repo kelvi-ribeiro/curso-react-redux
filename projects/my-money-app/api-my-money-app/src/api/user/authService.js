dee43ecc68ded4281eef4b8816437c06f163a459
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-20 18:02:19 -0300

    Aula 176. Autenticação - backend (Parte 07)

--- projects/my-money-app/api-my-money-app/src/api/user/authService.js
+++ projects/my-money-app/api-my-money-app/src/api/user/authService.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
+
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
 
@@ -15,6 +16,7 @@ const sendErrorsFromDB = (res, dbErrors) => {
 const login = (req, res, next) => {
     const email = req.body.email || ''
     const password = req.body.password || ''
+
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
@@ -32,11 +34,52 @@ const login = (req, res, next) => {
 
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
-    jwt.verify(token, env.authSecret, function(err, decoded) {
-    return res.status(200).send({valid: !err})
+
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+        return res.status(200).send({ valid: !err })
     })
-  }
+}
 
+const signup = (req, res, next) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
 
-  
-  
+    if (!email.match(emailRegex)) {
+        return res.status(400).send({ errors: ['O e-mail informado está inválido'] })
+    }
+
+    if (!password.match(passwordRegex)) {
+        return res.status(400).send({
+            errors: [
+                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$ %) e tamanho entre 6-20."
+            ]
+        })
+    }
+
+    const salt = bcrypt.genSaltSync()
+    const passwordHash = bcrypt.hashSync(password, salt)
+    if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({ errors: ['Senhas não conferem.'] })
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user) {
+            return res.status(400).send({ errors: ['Usuário já cadastrado.'] })
+        } else {
+            const newUser = new User({ name, email, password: passwordHash })
+            newUser.save(err => {
+                if (err) {
+                    return sendErrorsFromDB(res, err)
+                } else {
+                    login(req, res, next)
+                }
+            })
+        }
+    })
+}
+
+module.exports = { login, signup, validateToken }
