0f2eaffde8bda3f62dd85d2970c593639dd38258
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-20 18:05:08 -0300

    Aula 177. Autenticação - Backend (Parte 08)

--- projects/my-money-app/api-my-money-app/src/config/routers.js
+++ projects/my-money-app/api-my-money-app/src/config/routers.js
@@ -1,12 +1,27 @@
 const express = require('express')
-module.exports = function(server){
-    // URL Base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router )
+const auth = require('./auth')
 
-     // Rotas de Ciclo da Pagamento
+module.exports = function (server) {
 
-     const billingCycleService = require('../api/services/billingCycleService')
+    /*
+     * Rotas protegidas por Token JWT
+     */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-     billingCycleService.register(router, '/billingCycles')
+    protectedApi.use(auth)
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /*
+     * Rotas abertas
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
