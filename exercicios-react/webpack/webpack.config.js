643c242e397a162545b82d64841a56b690ed295f
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-25 02:30:10 -0300

    Aula 233. Carregando arquivos de imagens

--- exercicios-react/webpack/webpack.config.js
+++ exercicios-react/webpack/webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
                 'css-loader', // interpreta @import, url()...
                 'sass-loader',
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
             }
         ]
     }
