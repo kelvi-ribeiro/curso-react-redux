60d808c26ef6f27f205e389a5057008772f20e94
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-27 14:55:49 -0300

    Implementando logica para saber se o menu esta ativo

--- projects/register-user/register-user-app/src/components/template/NavItem.jsx
+++ projects/register-user/register-user-app/src/components/template/NavItem.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import FontAwesomeIcon from './FontAwesomeIcon';
 
-export default props =>                    
+export default props => {
+        const hash = window.location.hash        
+        const isActive = hash.substring(hash.indexOf('#') + 1,hash.length) === props.url
+        return (
         <Link to={props.url}>
-        <FontAwesomeIcon icon={props.icon}/> {props.nome}
-        </Link>        
-        
-    
+                
+        <FontAwesomeIcon icon={props.icon} /> {props.nome}
+        </Link>
+        )
+}
