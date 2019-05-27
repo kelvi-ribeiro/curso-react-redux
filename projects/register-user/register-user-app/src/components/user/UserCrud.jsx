a0d3870609dcf1477aada2ec6558c2c83158f265
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-27 11:56:04 -0300

    Aula 244. Componente Cadastro Usuário. Parte 02

--- projects/register-user/register-user-app/src/components/user/UserCrud.jsx
+++ projects/register-user/register-user-app/src/components/user/UserCrud.jsx
@@ -1,5 +1,6 @@
- import React, { Component } from 'react'
+ import React, { Component } from 'react' 
  import Main from '../template/Main'
+ import axios from 'axios'
 
  const headerProps = {
      icon:'users',
@@ -7,7 +8,42 @@
      subtitle:'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
  }
 
+ const baseUrl = 'http://localhost:3001/users'
+
+ const initialState = {
+     user:{name:'', email:'' },
+     list:[]
+ }
+
+
+
+
+
  export default class UserCrud extends Component {
+     state = {...initialState}
+
+     clear(){
+         this.setState({ user:initialState.user })
+     }
+
+     save(){
+         const user = this.state.user
+         const method = user.id ? 'put' : 'post'
+         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+         axios[method](url, user)
+         .then(resp => {
+             const list = this.getUptedList(resp.data)
+             this.setState({user:initialState.user, list})
+         })
+     }
+
+     getUptedList(user){
+         const list = this.state.list.filter( user => user.id !== user.id)
+         list.unshift(user)
+         return list
+
+     }
+
      render(){
          return (
             <Main {...headerProps}>
