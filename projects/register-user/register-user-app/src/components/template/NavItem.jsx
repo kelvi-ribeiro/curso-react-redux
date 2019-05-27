3323187722d2370033eb6748db8a8db34557712a
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-05-27 15:01:42 -0300

    Colocando classe 'active', caso o menu esteja ativo

--- projects/register-user/register-user-app/src/components/template/NavItem.jsx
+++ projects/register-user/register-user-app/src/components/template/NavItem.jsx
@@ -6,7 +6,7 @@ export default props => {
         const hash = window.location.hash        
         const isActive = hash.substring(hash.indexOf('#') + 1,hash.length) === props.url
         return (
-        <Link to={props.url}>
+        <Link to={props.url} className={isActive && 'active'}>
                 
         <FontAwesomeIcon icon={props.icon} /> {props.nome}
         </Link>
