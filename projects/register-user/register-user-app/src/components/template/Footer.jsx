f1b6a34a0338e7ea4ec6d7594caa23f0fcc83996
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-27 01:14:00 -0300

    Aula 239. Componente Rodapé

--- projects/register-user/register-user-app/src/components/template/Footer.jsx
+++ projects/register-user/register-user-app/src/components/template/Footer.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+           Desenvolvido com <i className="fa fa-heart text-danger"></i> Por &nbsp; 
+           <strong><span className="text-danger">K</span>
+           elvi&nbsp; 
+           <span className="text-danger">
+           R
+           </span>
+           ibeiro</strong>
+        </span>
     </footer>
